f25cf83d86ab730f5dc293673114bfe4c1382c51
Technickel-Dev <bradlabrador@hotmail.com>
2022-02-26 03:51:01 -0500

    Downloading SVG

--- src/lib/output.svelte
+++ src/lib/output.svelte
@@ -1,7 +1,31 @@
+<script>
+	let svgEl;
+
+	const downloadSVG = () => {
+		// Create a blob from the SVG code
+		const svg = svgEl.outerHTML;
+		const blob = new Blob([svg.toString()]);
+
+		// Create a temporary link to initiate the SVG file download
+		const tmpEl = document.createElement('a');
+		tmpEl.download = 'output.svg';
+		tmpEl.href = window.URL.createObjectURL(blob);
+		tmpEl.click();
+		tmpEl.remove();
+	};
+</script>
+
+<button class="bg-yellow-500 text-white py-2 px-4 rounded mr-4" on:click={downloadSVG}>
+	Download
+</button>
+
 <svg
+	bind:this={svgEl}
 	class="border-2"
 	width="350"
 	height="350"
 	viewBox="0 0 350 350"
 	xmlns="http://www.w3.org/2000/svg"
-/>
+>
+	<rect x="0" y="0" width="350" height="350" fill="green" />
+</svg>
