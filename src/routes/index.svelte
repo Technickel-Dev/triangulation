4a91cb105170f03ef61111beb1a3611826df87a3
Technickel-Dev <bradlabrador@hotmail.com>
2022-02-26 03:30:30 -0500

    Place point on canvas

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,36 @@
 <script>
-	const getCursorPosition = (event) => {
-		const rect = event.currentTarget.getBoundingClientRect();
-		const x = event.clientX - rect.left;
-		const y = event.clientY - rect.top;
-		console.log('x: ' + x + ' y: ' + y);
+	let drawCanvas;
+	let pointRadius = 10;
+
+	const getCanvasCursorPosition = (e) => {
+		const rect = e.currentTarget.getBoundingClientRect();
+		const x = e.clientX - rect.left;
+		const y = e.clientY - rect.top;
+		return { x, y };
+	};
+
+	const placePoint = (e) => {
+		// Setup variables for circle
+		let { x, y } = getCanvasCursorPosition(e);
+		const ctx = drawCanvas.getContext('2d');
+		const START_ANGLE = 0;
+		const END_ANGLE = 2 * Math.PI;
+
+		// Create and place circle
+		ctx.beginPath();
+		ctx.arc(x, y, pointRadius, START_ANGLE, END_ANGLE);
+		ctx.fillStyle = 'green';
+		ctx.fill();
 	};
 </script>
 
+<input type="number" bind:value={pointRadius} />
+
 <canvas
-	id="draw"
+	bind:this={drawCanvas}
 	on:mousedown={(e) => {
-		getCursorPosition(e);
+		placePoint(e);
 	}}
-	width="500"
-	height="500"
+	width={500}
+	height={500}
 />
